b4cee2fae89b80575c632ba449b9d01b26b8d8cc
Stephen Arra <arraworld@gmail.com>
2022-09-29 11:19:28 -0500

    add route to get jwt from google access token

--- src/auth/google/google-oauth.controller.ts
+++ src/auth/google/google-oauth.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Req, UseGuards, Render } from '@nestjs/common';
+import { Controller, Get, Req, Query, UseGuards, Render } from '@nestjs/common';
 import { Request } from 'express';
 import { GoogleOauthGuard } from './google-oauth.guard';
+import { GoogleOauthService } from './google-oauth.service';
 import { JwtAuthService } from '../jwt/jwt-auth.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth/google')
 export class GoogleOauthController {
-  constructor(private jwtAuthService: JwtAuthService) {}
+  constructor(
+    private jwtAuthService: JwtAuthService,
+    private configService: ConfigService,
+    private readonly googleOauthService: GoogleOauthService,
+  ) {}
 
   @Get()
   @UseGuards(GoogleOauthGuard)
@@ -18,6 +24,13 @@ export class GoogleOauthController {
   @Render('authenticated')
   async googleAuthRedirect(@Req() req: Request) {
     const { accessToken } = this.jwtAuthService.login(req.user);
+    return { token: accessToken };
+  }
+
+  @Get('token')
+  async getToken(@Query('code') code: string) {
+    const user = await this.googleOauthService.getUserFromToken(code);
+    const { accessToken } = this.jwtAuthService.login(user);
     return { token: accessToken };
   }
 }
